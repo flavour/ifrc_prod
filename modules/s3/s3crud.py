7e9f52e5a8fbce2b4d1f005143075be3074a69aa
Fran Boon <fran@aidiq.com>
2014-08-08 21:27:24 +0200

    Backport fixes

--- modules/s3/s3crud.py
+++ modules/s3/s3crud.py
@@ -2705,7 +2705,6 @@ class S3CRUD(S3Method):
             # Extract data for embedded form from post_vars
             post_vars = request.post_vars
             form_vars = Storage(table._filter_fields(post_vars))
-            form_vars[table._id.name] = selected
             
             # Pass values through validator to convert them into db-format
             for k in form_vars:
@@ -2716,6 +2715,7 @@ class S3CRUD(S3Method):
             _form = Storage(vars = form_vars, errors = Storage())
             if _form.vars:
                 if selected:
+                    form_vars[table._id.name] = selected
                     # Onvalidation
                     onvalidation = get_config("update_onvalidation") or \
                                    get_config("onvalidation")
@@ -2738,6 +2738,7 @@ class S3CRUD(S3Method):
                                get_config("onaccept")
                     callback(onaccept, _form, tablename=component)
                 else:
+                    form_vars.pop(table._id.name, None)
                     # Onvalidation
                     onvalidation = get_config("create_onvalidation") or \
                                    get_config("onvalidation")
